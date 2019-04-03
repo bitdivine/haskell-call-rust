6df623a5132418e383c9c6b3b8417d026ed2564e
Max <max@winning.black>
2019-04-03 20:08:17 +0200

    Noddy call rust from haskell

---
+++ src/FLib.hs
@@ -0,0 +1,7 @@
+module FLib where
+
+import Foreign.C.Types
+import Foreign.C.String
+
+foreign import ccall "double_input" doubleInput :: CInt -> CInt
+foreign import ccall unsafe "print_string" printString :: CString -> IO ()
